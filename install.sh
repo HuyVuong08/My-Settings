6bb8584a5d1ed24724834198d964fb1c72d57a1a
HuyVuong08 <huy.vuongle123@gmail.com>
2021-03-09 03:32:21 +0700

    Remove interupt features due to many bugs

--- install.sh
+++ install.sh
@@ -3,117 +3,85 @@
 # Save working directory
 WORKDIR="$(pwd)"
 
-# Define signal handler and its variable
-allowAbort=true;
-myInterruptHandler() {
-    if $allowAbort; then
-        exit 1;
-    fi;
-}
+while true; do
+    read -p "Do you wish to install latex suite?[Yes][No]" yn
+    case $yn in
+        [Yy]* ) latex=TRUE; break;;
+        [Nn]* ) latex=FALSE; break;;
+        * ) echo "Please choose yes or no.";;
+    esac
+done
 
-# Register signal handler
-trap myInterruptHandler SIGINT;
+while true; do
+    read -p "Do you wish to install Paint?[Yes][No]" yn
+    case $yn in
+        [Yy]* ) paint=TRUE; break;;
+        [Nn]* ) paint=FALSE; break;;
+        * ) echo "Please choose yes or no.";;
+    esac
+done
 
-# Wrapper
-wrapInterruptable() {
-    # disable the abortability of the script
-    allowAbort=false;
-    # run the passed arguments 1:1
-    "$@";
-    # save the returned value
-    local ret=$?;
-    # make the script abortable again
-    allowAbort=true;
-    # and return
-    return "$ret";
-}
+while true; do
+    read -p "Do you wish to install Sreen Recorder?[Yes][No]" yn
+    case $yn in
+        [Yy]* ) recorder=TRUE; break;;
+        [Nn]* ) recorder=FALSE; break;;
+        * ) echo "Please choose yes or no.";;
+    esac
+done
 
-# Setup procedure
-setup () {
+while true; do
+    read -p "Do you wish to rename Firefox to Safari?[Yes][No]" yn
+    case $yn in
+        [Yy]* ) safari=TRUE; break;;
+        [Nn]* ) safari=FALSE; break;;
+        * ) echo "Please choose yes or no.";;
+    esac
+done
 
-    while true; do
-        read -p "Do you wish to install latex suite?[Yes][No]" yn
-        case $yn in
-            [Yy]* ) latex=TRUE; break;;
-            [Nn]* ) latex=FALSE; break;;
-            * ) echo "Please choose yes or no.";;
-        esac
-    done
+while true; do
+    read -p "This process might takes a long time and should not be interupted. Aure you sure to proceed?[Yes][No]" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit 1;;
+        * ) echo "Please choose yes or no.";;
+    esac
+done
 
-    while true; do
-        read -p "Do you wish to install Paint?[Yes][No]" yn
-        case $yn in
-            [Yy]* ) paint=TRUE; break;;
-            [Nn]* ) paint=FALSE; break;;
-            * ) echo "Please choose yes or no.";;
-        esac
-    done
+#-------------------------------------------
 
-    while true; do
-        read -p "Do you wish to install Sreen Recorder?[Yes][No]" yn
-        case $yn in
-            [Yy]* ) recorder=TRUE; break;;
-            [Nn]* ) recorder=FALSE; break;;
-            * ) echo "Please choose yes or no.";;
-        esac
-    done
+## Install config
+#cd $WORKDIR/config
+#./install.sh
+#cd $WORKDIR
 
-    while true; do
-        read -p "Do you wish to rename Firefox to Safari?[Yes][No]" yn
-        case $yn in
-            [Yy]* ) safari=TRUE; break;;
-            [Nn]* ) safari=FALSE; break;;
-            * ) echo "Please choose yes or no.";;
-        esac
-    done
+#-------------------------------------------
 
-    while true; do
-        read -p "This process might takes a long time and should not be interupted. Aure you sure to proceed?[Yes][No]" yn
-        case $yn in
-            [Yy]* ) break;;
-            [Nn]* ) exit 1;;
-            * ) echo "Please choose yes or no.";;
-        esac
-    done
+# Install Frequently Used Apps
 
-    #-------------------------------------------
+if [ $latex = "TRUE" ]
+then
+    echo latex
 
-    ## Install config
-    #cd $WORKDIR/config
-    #./install.sh
-    #cd $WORKDIR
+fi
 
-    #-------------------------------------------
+if [ $paint = "TRUE" ]
+then
+    echo paint
+fi
 
-    # Install Frequently Used Apps
+if [ $recorder = "TRUE" ]
+then
+    echo recorder
+fi
 
-    if [ $latex = "TRUE" ]
-    then
-        echo latex
+if [ $safari = "TRUE" ]
+then
+    echo safari
+fi
 
-    fi
+# Install Renamed Apps
 
-    if [ $paint = "TRUE" ]
-    then
-        echo paint
-    fi
+#-------------------------------------------
 
-    if [ $recorder = "TRUE" ]
-    then
-        echo recorder
-    fi
-
-    if [ $safari = "TRUE" ]
-    then
-        echo safari
-    fi
-
-    # Install Renamed Apps
-
-    #-------------------------------------------
-
-    # Install Run Diablog Apps
-}
-
-# Call Setup Procedure
-wrapInterruptable setup
+# Install Run Diablog Apps
