0319805bfc5139eea6bf5da9123828a0fea6f7d9
HuyVuong08 <huy.vuongle123@gmail.com>
2021-03-09 18:04:09 +0700

    Remove interupt feature

--- config/install.sh
+++ config/install.sh
@@ -66,6 +66,7 @@ sudo gem install mdless
 # Install TeamViewer
 echo "Installing TeamViewer ..."
 cd $HOME
+rm teamviewer_amd64.deb
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 sudo apt-get install -y ./teamviewer_amd64.deb
 cd $WORKDIR
