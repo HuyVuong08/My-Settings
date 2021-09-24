e93ad6f6d87db8a15e1510c8822fdf1f9c694be4
HuyVuong08 <huy.vuongle123@gmail.com>
2021-09-24 23:24:03 +0700

    Add script to automatically set key binding for Flameshot

--- Frequently-Used-Apps/install/flameshot-install.sh
+++ Frequently-Used-Apps/install/flameshot-install.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-#Install Flameshot
-echo "Installing Flameshot for screenshot ..."
+# Install Flameshot
+echo "Installing Flameshot for screenshot..."
 sudo apt-get install -y flameshot
+
+#-------------------------------------------
+
+echo "Setting shortcut-key for launching Flameshot to take a screenshot..."
+
+# Release the PrtScr binding
+gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot '[]'
+
+# Set new custom binding
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+
+# Set name
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'flameshot'
+
+# Set command
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command '/usr/bin/flameshot gui'
+
+# Set binding
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding 'Print'
