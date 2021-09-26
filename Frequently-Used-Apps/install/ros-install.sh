f81424f0a98fc6d94f56794891f8638a1f411079
HuyVuong08 <49582049+HuyVuong08@users.noreply.github.com>
2021-09-26 16:03:21 +0700

    Fix spelling error in ros-install.sh

--- Frequently-Used-Apps/install/ros-install.sh
+++ Frequently-Used-Apps/install/ros-install.sh
@@ -25,7 +25,7 @@ echo 'Setting Up sources.list...'
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 # Setup Keys
-echo 'Setting Up Yeys...'
+echo 'Setting Up Keys...'
 sudo apt-get install -y curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
