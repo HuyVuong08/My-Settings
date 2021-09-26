f3708b416ee6c362626d08f8d7153a60334646c0
HuyVuong08 <huy.vuongle123@gmail.com>
2021-09-26 19:37:56 +0700

    Fix git clone WhiteSur-gtk in wrong directory issue

--- MacOS-Themes/gtk-and-icon-theme-install.sh
+++ MacOS-Themes/gtk-and-icon-theme-install.sh
@@ -24,20 +24,26 @@ sudo apt-get install -y git
 
 # Install GTK themes
 echo 'Installing MacOS Big Sur GTK Themes...'
-git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
-sudo $WORKDIR/WhiteSur-gtk-theme/install.sh --dest '/usr/share/themes/' --opacity 'normal' --monterey  --highdefinition --roundedmaxwindow --nautilus-style 'stable'
-sudo $WORKDIR/WhiteSur-gtk-theme/tweaks.sh --firefox 'monterey' --color 'dark' --theme 'default' --panel-size 'default' --icon 'standard'
-sudo $WORKDIR/WhiteSur-gtk-theme/tweaks.sh --gdm 'default'
+(
+    cd $WORKDIR
+    git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+    sudo $WORKDIR/WhiteSur-gtk-theme/install.sh --dest '/usr/share/themes/' --opacity 'normal' --monterey  --highdefinition --roundedmaxwindow --nautilus-style 'stable'
+    sudo $WORKDIR/WhiteSur-gtk-theme/tweaks.sh --firefox 'monterey' --color 'dark' --theme 'default' --panel-size 'default' --icon 'standard'
+    sudo $WORKDIR/WhiteSur-gtk-theme/tweaks.sh --gdm 'default'
 
-# Remove WhiteSur-gtk-theme folder
-# rm -rf WhiteSur-gtk-theme
+    # Remove WhiteSur-gtk-theme folder
+    # rm -rf WhiteSur-gtk-theme
+)
 
 #-------------------------------------------
 
 # Install Icon themes
 echo 'Installing MacOS Big Sur Icon Themes...'
-git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
-sudo $WORKDIR/WhiteSur-icon-theme/install.sh -d '/usr/share/icons/'
+(
+    cd $WORKDIR
+    git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
+    sudo $WORKDIR/WhiteSur-icon-theme/install.sh -d '/usr/share/icons/'
 
-# Remove WhiteSur-icon-theme folder
-# rm -rf WhiteSur-icon-theme
+    # Remove WhiteSur-icon-theme folder
+    # rm -rf WhiteSur-icon-theme
+)
