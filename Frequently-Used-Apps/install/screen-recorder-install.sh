332e5d746b65e3e5b368a5db49c4924657292362
HuyVuong08 <huy.vuongle123@gmail.com>
2021-11-05 00:30:15 +0700

    Install Voko Screen for screen recording in ubuntu 16

--- Frequently-Used-Apps/install/screen-recorder-install.sh
+++ Frequently-Used-Apps/install/screen-recorder-install.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository -y ppa:ubuntuhandbook1/apps
 #Update the repository
 sudo apt-get -y update
 #Install vokoScreen-ng
-sudo apt-get install -y vokoscreen-ng
+sudo apt-get install -y vokoscreen-ng || sudo apt-get install -y vokoscreen
 
 #Auto remove
 sudo apt-get -y autoremove
