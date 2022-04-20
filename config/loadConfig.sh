3322d5fdceccdc0445bd198315cd5a0603ca0e4f
HuyVuong08 <huy.vuongle123@gmail.com>
2022-04-20 21:06:45 +0700

    source bash_aliases and bash_functions

--- config/loadConfig.sh
+++ config/loadConfig.sh
@@ -18,13 +18,17 @@ OverwriteCopy () {
     #Load .bash_aliases
     echo Loading .bash_aliases...
     cp -v $WORKDIR/.bash_aliases $HOME && source $HOME/.bashrc
+    if ! grep -Fxq "    source .bash_aliases" $HOME/.bashrc
+    then
+        echo -e "\nif [ -f ~/.bash_aliases ]; then\n    source.bash_aliases\nfi" >> $HOME/.bashrc
+    fi
 
     #Load .bash_functions
     echo Loading .bash_functions...
     cp -v $WORKDIR/.bash_functions $HOME && source $HOME/.bashrc
-    if ! grep -Fxq "    . ~/.bash_functions" $HOME/.bashrc
+    if ! grep -Fxq "    source .bash_functions" $HOME/.bashrc
     then
-        echo -e "\nif [ -f ~/.bash_functions ]; then\n    . ~/.bash_functions\nfi" >> $HOME/.bashrc
+        echo -e "\nif [ -f ~/.bash_functions ]; then\n    source.bash_functions\nfi" >> $HOME/.bashrc
     fi
 
     #Load battery-status.sh
