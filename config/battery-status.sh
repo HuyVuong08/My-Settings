50c4594db2e4c3ad61becd54f9057a0c8e7dd38c
HuyVuong08 <huy.vuongle123@gmail.com>
2022-05-12 17:52:19 +0700

    Fix if syntax error in battery-status.sh

--- config/battery-status.sh
+++ config/battery-status.sh
@@ -5,15 +5,15 @@ do
     battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
     if on_ac_power
     then
-        if [[ $battery_level -ge 80 ]]
+        if [ $battery_level -ge 80 ]
         then
-            notify-send --urgency=normal --expire-time=20000 --hint int:transient:1 "Battery Level: ${battery_level}%" "Please unplug the charging cable."
+            notify-send --urgency=low --expire-time=20000 --hint int:transient:1 "Battery Level: ${battery_level}%" "Please unplug the charging cable."
             #paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
         fi
     else
-        if [[ $battery_level -le 40 ]]
+        if [ $battery_level -le 40 ]
         then
-            notify-send --urgency=normal --expire-time=20000 --hint int:transient:1 "Low Battery Level: ${battery_level}%" "Please plug in the charging cable."
+            notify-send --urgency=low --expire-time=20000 --hint int:transient:1 "Low Battery Level: ${battery_level}%" "Please plug in the charging cable."
             #paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
         fi
     fi
