351c8dfaa3c89e3043dc09c00460164949279939
HuyVuong08 <huy.vuongle123@gmail.com>
2023-05-21 09:00:39 +0800

    update install scripts to latest albert version

--- MacOS-Themes/albert-install.sh
+++ MacOS-Themes/albert-install.sh
@@ -19,8 +19,8 @@ sudo apt-get install -y wget
 # Install Albert with MacOS Big Sur Spotlight theme
 echo 'Installing MacOS Big Sur Albert Spotlight...'
 curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
-echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
-sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
+echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_21.10/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_21.10/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
 sudo apt-get -y update
 sudo apt-get install -y albert
 
